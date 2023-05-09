5c486bf7a9cd6bbbc4754439019bf6f9686eb3ba
ARNAB BASAK <98828838+Arnab2012@users.noreply.github.com>
2023-05-09 19:59:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,13 @@
 import streamlit as st
+from flask import request
 import requests
 
 st.header('Friend')
 
-data = st.text_input('Enter your message here')
+url = "your_dialogflow_webhook_url_here"
+headers = {"Content-Type": "application/json"}
 
-url = 'https://arnab2012-friend-chatbot-app-vkyz89.streamlit.app/'
-headers = {'Content-Type': 'application/json'}
+response = requests.post(url, headers=headers, json=request.get_json())
 
-data = {
-    'message': data,
-    'sender': 'streamlit'
-}
-
-response = requests.post(url, headers=headers, json=data)
-
-if response.ok:
-    response_data = response.json()
-    st.write(response_data['response'])
-else:
-    st.write('Error:', response.status_code)
+data = response.json()
+print(str(data))
